5444c5034da61a8491ece6f92eb910172fff6669
WarManWarrior <141147536+WarManWarrior@users.noreply.github.com>
2024-12-06 16:19:57 +0530

    Template summerization
    Final template summerization

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -2,9 +2,8 @@ function App() {
 
   return (
     <div className='w-screen h-screen bg-red-400'>
-      
     </div>
   )
-}
+};
 
 export default App
--- website/src/main.jsx
+++ website/src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from "react-dom"
 import App from './App.jsx'
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const Root = ReactDOM.createRoot(document.getElementById("root"));
+
+Root.render(<App></App>);
