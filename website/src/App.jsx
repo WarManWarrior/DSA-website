2190a68dcaee33571f5005ffa108ba57332e5cf0
WarManWarrior <141147536+WarManWarrior@users.noreply.github.com>
2024-12-15 00:06:32 +0530

    Initial Bubble sort
    Initial version of bubble sort algorithm added

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./assets/pages/home";
 import InsertionSortVisualizer from "./assets/algorithms/insertion_sort";
 import LinearSearch from "./assets/algorithms/linearsearch";
+import BubbleSortVisualizer from "./assets/algorithms/bubble_sort";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/insertion" element={<InsertionSortVisualizer />} />
-          <Route path="/linearsearch" element={<LinearSearch />} />
+          <Route path="/linearsearch" element={<BubbleSortVisualizer />} />
         </Routes>
       </Router>
     </div>
