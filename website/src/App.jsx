5a271a7dabfaf34ac3b5a8638d68b4930350d9b9
Satyam Kumar Choudhary <choudharysatyam951478@gmail.com>
2024-12-17 23:39:35 +0530

    Code editor page component creation

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -8,6 +8,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StrassenMatrixVisualization from "./assets/algorithms/Strassen_Matrix";
 import HuffmanCodingVisualization from "./assets/algorithms/Huffman_coding";
 import KnapsackVisualization from "./assets/algorithms/Kanpsack_Greedy";
+import CodeEditor from "./assets/page_components/CodeEditor";
+
+
+
 
 function App() {
   return (
@@ -16,7 +20,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/insertion" element={<InsertionSortVisualizer />} />
-          <Route path="/linearsearch" element={<MergeSortVisualizer />} />
+          <Route path="/linearsearch" element={<CodeEditor />} />
         </Routes>
       </Router>
     </div>
