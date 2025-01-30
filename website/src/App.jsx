3d2ea73bc698ec818c557982bb77de6f561060bc
Satyam Kumar Choudhary <choudharysatyam951478@gmail.com>
2025-01-30 20:00:23 +0530

    Randomized Quick Sort Final Draft

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/insertion" element={<InsertionSortVisualizer />} />
-          <Route path="/linearsearch" element={<QuickSort/>} />
+          <Route path="/linearsearch" element={<RandomizedQuickSort/>} />
         </Routes>
       </Router>
     </div>
