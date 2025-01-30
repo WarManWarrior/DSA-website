008f4fce442cd86b063d41a3693b7a46a7c62584
WarManWarrior <141147536+WarManWarrior@users.noreply.github.com>
2025-01-30 21:06:48 +0530

    non significant

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/insertion" element={<InsertionSortVisualizer />} />
-          <Route path="/linearsearch" element={<ExperimentsPage />} />
+          <Route path="/listofexp" element={<ExperimentsPage />} />
         </Routes>
       </Router>
     </div>
